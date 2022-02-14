85d36e33b2e1edd05923e9d34bdd0d4dc16f5ef8
BlindArcher <robbe.decraemer@ucll.be>
2022-02-14 15:49:42 +0100

    FahrenheitTemperatureScale

--- TemperatureConverter/View/TemperatureScale.cs
+++ TemperatureConverter/View/TemperatureScale.cs
@@ -38,4 +38,19 @@
             return temperature + 273.15;
         }
     }
+
+    public class FahrenheitTemperatureScale : ITemperatureScale
+    {
+        public string Name => "Fahrenheit";
+
+        public double ConvertFromKelvin(double temperature)
+        {
+            return temperature * 1.8 - 459.67; ;
+        }
+
+        public double ConvertToKelvin(double temperature)
+        {
+            return (temperature + 459.67) / 1.8;
+        }
+    }
 }
