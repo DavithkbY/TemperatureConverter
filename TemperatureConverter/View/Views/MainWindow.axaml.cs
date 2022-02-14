06833a8913bc726eb21f160e9d000a640e279382
BlindArcher <robbe.decraemer@ucll.be>
2022-02-14 13:52:28 +0100

    added second text box

--- TemperatureConverter/View/Views/MainWindow.axaml.cs
+++ TemperatureConverter/View/Views/MainWindow.axaml.cs
@@ -22,26 +22,28 @@ namespace View.Views
 
         private void ConvertToCelsius(object sender, RoutedEventArgs e)
         {
-            var textBox = this.FindControl<TextBox>("textBox");
+            var celsiusTextBox = this.FindControl<TextBox>("celsiusTextBox");
+            var fahrenheitTextBox = this.FindControl<TextBox>("fahrenheitTextBox");
 
-            var fahrenheitString = textBox.Text;
+            var fahrenheitString = fahrenheitTextBox.Text;
             var fahrenheit = double.Parse(fahrenheitString);
             var celsius = (fahrenheit - 32) / 1.8;
             var celsiusString = celsius.ToString();
 
-            textBox.Text = celsiusString;
+            celsiusTextBox.Text = celsiusString;
         }
 
         private void ConvertToFahrenheit(object sender, RoutedEventArgs e)
         {
-            var textBox = this.FindControl<TextBox>("textBox");
+            var celsiusTextBox = this.FindControl<TextBox>("celsiusTextBox");
+            var fahrenheitTextBox = this.FindControl<TextBox>("fahrenheitTextBox");
 
-            var celsiusString = textBox.Text;
+            var celsiusString = celsiusTextBox.Text;
             var celsius = double.Parse(celsiusString);
             var fahrenheit = celsius * 1.8 + 32;
             var fahrenheitString = fahrenheit.ToString();
 
-            textBox.Text = fahrenheitString;
+            fahrenheitTextBox.Text = fahrenheitString;
         }
     }
 }
