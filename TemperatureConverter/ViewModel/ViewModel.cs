dbacc4bfab964f13dbbd3f5c22532481187d3075
BlindArcher <robbe.decraemer@ucll.be>
2022-02-14 16:42:58 +0100

    increment

--- TemperatureConverter/ViewModel/ViewModel.cs
+++ TemperatureConverter/ViewModel/ViewModel.cs
@@ -1,7 +1,8 @@
 ﻿using Cells;
 using Model;
+using System;
+using System.Windows.Input;
 using System.Collections.Generic;
-using System.ComponentModel;
 
 namespace ViewModel
 {
@@ -46,9 +47,33 @@ namespace ViewModel
             this.temperatureScale = temperatureScale;
 
             this.Temperature = this.parent.TemperatureInKelvin.Derive(kelvin => temperatureScale.ConvertFromKelvin(kelvin), t => temperatureScale.ConvertToKelvin(t));
+            this.Increment = new IncrementCommand(this.Temperature);
         }
 
         public string Name => temperatureScale.Name;
         public Cell<double> Temperature { get; }
+        public ICommand Increment { get; }
+    }
+
+    public class IncrementCommand : ICommand
+    {
+        private readonly Cell<double> cell;
+
+        public IncrementCommand(Cell<double> cell)
+        {
+            this.cell = cell;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            cell.Value = Math.Round(cell.Value + 1);
+        }
     }
 }
